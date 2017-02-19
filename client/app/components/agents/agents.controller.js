ee07c29d7a056208933c1830d27aaef19298f177
Krishna <Krishna>
2017-02-19 22:21:25 +1100

    Final Commit

--- client/app/components/agents/agents.controller.js
+++ client/app/components/agents/agents.controller.js
@@ -8,23 +8,19 @@ class AgentsController {
     this.loading = false;
   }
   findAgents() {
-    var vm = this;
+    let vm = this;
     vm.showHeading = true;
     vm.loading = true;
     vm.results = false;
     vm.$http.get("https://api.ratemyagent.com.au/autosearch/agents?SearchTerm=" + vm.details.agent)
-      .then(function (res) {
+      .then((response) => {
         vm.loading = false;
-        if (res.data.Results.length === 0) {
+        if (response.data.Results.length === 0) {
           vm.results = true;
           vm.showHeading = false;
         }
-        vm.agents = res.data.Results;
-        console.log(res);
+        vm.agents = response.data.Results;
       });
   }
-
-
 }
-
 export default AgentsController;
--- client/app/components/agents/agents.spec.js
+++ client/app/components/agents/agents.spec.js
@@ -4,35 +4,41 @@ import AgentsComponent from './agents.component';
 import AgentsTemplate from './agents.html';
 
 describe('Agents', () => {
-  let $rootScope, makeController;
+  let $rootScope, makeController,$state,$location,$compile;
 
   beforeEach(window.module(AgentsModule));
-  beforeEach(inject((_$rootScope_) => {
-    $rootScope = _$rootScope_;
+  beforeEach(inject(($injector) => {
+    $rootScope = $injector.get('$rootScope');
     makeController = () => {
       return new AgentsController();
     };
+     $state = $injector.get('$state');
+    $location = $injector.get('$location');
+    $compile = $injector.get('$compile');
   }));
 
   describe('Module', () => {
     // top-level specs: i.e., routes, injection, naming
+     it('Agent component should be visible when navigates to /agent', () => {
+      $location.url('/agents');
+      $rootScope.$digest();
+      expect($state.current.component).to.eq('agents');
+    });
   });
 
   describe('Controller', () => {
     // controller specs
-    it('has a name property [REMOVE]', () => { // erase if removing this.name from the controller
+    it('has the properties in Agent Controller', () => { // erase if removing this.name from the controller
       let controller = makeController();
       expect(controller).to.have.property('name');
+      expect(controller).to.have.property('showHeading');
+      expect(controller).to.have.property('loading');
+      expect(controller).to.have.property('details');
+         
     });
   });
 
-  describe('Template', () => {
-    // template specs
-    // tip: use regex to ensure correct bindings are used e.g., {{  }}
-    it('has name in template [REMOVE]', () => {
-      expect(AgentsTemplate).to.match(/{{\s?\$ctrl\.name\s?}}/g);
-    });
-  });
+
 
   describe('Component', () => {
       // component/directive specs
